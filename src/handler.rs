5a1ee7b6d4620342bf0af37721c161905e5f706f
Hao Guan <10684225+hguandl@users.noreply.github.com>
2024-03-27 16:37:07 +0800

    Add basic parser for danmu

--- src/handler.rs
+++ src/handler.rs
@@ -1,6 +1,7 @@
 use std::future::Future;
 
 use serde::Deserialize;
+use serde_json::Value;
 use tracing::{debug, info};
 
 use crate::{sub::SubReply, Error, Result};
@@ -48,9 +49,53 @@ pub enum LiveMessage {
     #[serde(rename = "PREPARING")]
     Preparing,
     #[serde(rename = "DANMU_MSG")]
-    Danmu,
+    Danmu(DanmuMessage),
     #[serde(rename = "INTERACT_WORD")]
     Interact,
     #[serde(other)]
     Other,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct DanmuMessage {
+    pub content: String,
+    pub uid: u64,
+    pub uname: String,
+}
+
+impl<'de> Deserialize<'de> for DanmuMessage {
+    fn deserialize<D>(deserializer: D) -> std::result::Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let root: Value = Deserialize::deserialize(deserializer)?;
+
+        let info = root["info"]
+            .as_array()
+            .ok_or(serde::de::Error::custom("cannot parse array `info`"))?;
+
+        let content = info[1]
+            .as_str()
+            .ok_or(serde::de::Error::custom("cannot parse str `info[1]`"))?
+            .to_string();
+
+        let user = info[2]
+            .as_array()
+            .ok_or(serde::de::Error::custom("cannot parse array `info[2]`"))?;
+
+        let uid = user[0]
+            .as_u64()
+            .ok_or(serde::de::Error::custom("cannot parse u64 `info[2][0]`"))?;
+
+        let uname = user[1]
+            .as_str()
+            .ok_or(serde::de::Error::custom("cannot parse str `info[2][1]`"))?
+            .to_string();
+
+        Ok(Self {
+            content,
+            uid,
+            uname,
+        })
+    }
 }
